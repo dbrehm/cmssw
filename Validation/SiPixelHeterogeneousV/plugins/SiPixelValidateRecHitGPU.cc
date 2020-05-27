eacd99c944186360594f7a0263ee87135f95b2cd
Derek <dbrehm@cern.ch>
2020-05-27 21:12:38 +0200

    First change to SiPixelValidateRecHitGPU.cc

--- Validation/SiPixelHeterogeneousV/plugins/SiPixelValidateRecHitGPU.cc
+++ Validation/SiPixelHeterogeneousV/plugins/SiPixelValidateRecHitGPU.cc
@@ -23,6 +23,10 @@
 #include "DQMServices/Core/interface/DQMEDAnalyzer.h"
 #include "DataFormats/GeometryVector/interface/GlobalPoint.h"
 
+#include "SimDataFormats/TrackingHit/interface/PSimHit.h"
+#include "SimTracker/TrackerHitAssociation/interface/TrackerHitAssociator.h" 
+#include "TrackingTools/TrackAssociator/interface/TrackDetectorAssociator.h"
+
 class SiPixelValidateRecHitGPU : public DQMEDAnalyzer {
 
 public:
@@ -39,6 +43,12 @@ private:
   cms::cuda::host::unique_ptr<float[]> m_store32;
   cms::cuda::host::unique_ptr<uint32_t[]> m_hitsModuleStart;
   
+  std::vector<PSimHit> vec_simhits_assoc;
+  std::vector<float> vec_simhits_x;
+  std::vector<float> vec_simhits_y;
+
+  TrackerHitAssociator *associate(0);
+
   std::string topFolderName_;
   MonitorElement* nHits;
   MonitorElement* hrecHit_Barrel_XY;
@@ -47,6 +57,10 @@ private:
   MonitorElement* hrecHit_Barrel_RZ;
   MonitorElement* hrecHit_Endcap_plus_RZ;
   MonitorElement* hrecHit_Endcap_minus_RZ;
+  MonitorElement* hrecHit_pull_X;
+  MonitorElement* hrecHit_pull_Y;
+  MonitorElement* hrecHit_residual_X;
+  MonitorElement* hrecHit_residual_Y;
 
 };
 
@@ -68,6 +82,11 @@ void SiPixelValidateRecHitGPU::bookHistograms(DQMStore::IBooker& ibooker, edm::R
   hrecHit_Barrel_RZ = ibooker.book2D( "RecHit_Barrel_RZ", "RecHit #rho vs. z", 900, -300, 300, 480, 0, 120 );
   hrecHit_Endcap_plus_RZ = ibooker.book2D( "RecHit_Endcap_plus_RZ", "RecHit #rho vs. z", 900, -300, 300, 480, 0, 120 );
   hrecHit_Endcap_minus_RZ = ibooker.book2D( "RecHit_Endcap_minus_RZ", "RecHit #rho vs. z", 900, -300, 300, 480, 0, 120 );
+
+  hrecHit_residual_X = ibooker.book1D("h_x", "X position residual; #Deltax (#mum)" , 100, -300, 300);
+  hrecHit_residual_Y = ibooker.book1D("h_y", "Y position residual; #Deltax (#mum)" , 100, -300, 300);
+  hrecHit_pull_X = ibooker.book1D("h_pullx", "X Pull; X Pull" , 100, -3, 3);
+  hrecHit_pull_Y = ibooker.book1D("h_pully", "Y Pull; Y Pull" , 100, -3, 3);
 
 }
 
@@ -107,6 +126,8 @@ void SiPixelValidateRecHitGPU::analyze(const edm::Event& iEvent, edm::EventSetup
 
   constexpr uint32_t MaxHitsInModule = gpuClustering::MaxHitsInModule;
   
+  associate = new TrackerHitAssociator(iEvent,trackerHitAssociatorConfig_);
+  
   for (auto DSViter = input.begin(); DSViter != input.end(); DSViter++) {
     unsigned int detid = DSViter->detId();
     DetId detIdObject(detid);
@@ -114,7 +135,7 @@ void SiPixelValidateRecHitGPU::analyze(const edm::Event& iEvent, edm::EventSetup
     auto gind = gDetUnit->index();
     const PixelGeomDetUnit* pixDet = dynamic_cast<const PixelGeomDetUnit*>(gDetUnit);
     assert(pixDet);
-    
+
     auto fc = m_hitsModuleStart[gind];
     auto lc = m_hitsModuleStart[gind + 1];
     auto nhits = lc - fc;
@@ -136,6 +157,43 @@ void SiPixelValidateRecHitGPU::analyze(const edm::Event& iEvent, edm::EventSetup
       
       GlobalPoint rechitPos = gDetUnit->surface().toGlobal(lp);
       
+      vec_simhits_assoc.clear();
+      vec_simhits_x.clear();
+      vec_simhits_y.clear();
+      vec_simhits_assoc = associate->associateHit(*geom);
+
+      int iSimHit = 0;
+
+      for (std::vector<PSimHit>::const_iterator m = vec_simhits_assoc.begin(); m < vec_simhits_assoc.end() && iSimHit < SIMHITPERCLMAX; ++m){
+          vec_simhits_x.push_back(( m->entryPoint().x() + m->exitPoint().x() ) / 2.0);
+          vec_simhits_y.push_back(( m->entryPoint().y() + m->exitPoint().y() ) / 2.0);
+          ++iSimHit;
+      }
+      
+      float dx(9999), dy(9999), pullx(9999), pully(9999);
+      int iKx(0), iKy(0);
+      for(int k=0; k<iSimHit; k++){
+          if(fabs(vec_simhits_x[k] - rechitPos.x()) < fabs(dx)){ 
+              dx = rechitPos.x() - vec_simhits_x[k];
+              pullx =  dx/(TMath::Sqrt(hit->localPositionError().xx()));
+              iKx = k;
+          }
+          if(fabs(vec_simhits_y[k] - rechitPos.y()) < fabs(dy)){ 
+              dy =  rechitPos.y() - vec_simhits_y[k];
+              pully =  dy/(TMath::Sqrt(hit->localPositionError().yy()));
+              iKy = k;
+          }
+      }
+      bool fill = (dx<9999) && (dy<9999);
+      if(fill){
+      //mult by 10000 to convert to microns
+          float to_microns = 1e4;
+          hrecHit_residual_X->Fill(to_microns * dx);
+          hrecHit_residual_Y->Fill(to_microns * dy);
+          if(hrecHit_pull_X != nullptr) hrecHit_pull_X->Fill(pullx);
+          if(hrecHit_pull_Y != nullptr) hrecHit_pull_Y->Fill(pully);
+      }
+
       if (gDetUnit->type().isBarrel()) {
 	hrecHit_Barrel_XY->Fill( rechitPos.x(), rechitPos.y() );
 	hrecHit_Barrel_RZ->Fill( rechitPos.z(), rechitPos.perp());
